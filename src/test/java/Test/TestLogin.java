c37fc5d93ec6c4941525b9293b547633d75d8edd
Aman <aman@gmail.cm>
2026-07-31 15:10:11 +0530

    Shubham update code from local to beta

--- src/test/java/Test/TestLogin.java
+++ src/test/java/Test/TestLogin.java
@@ -49,26 +49,6 @@ public class TestLogin extends Base {
 	public void LoginTestCorrectDetail() {
 		log.info("========== Starting Login Test ==========");
 		
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		System.out.println("Git hub update for testing");
-		
-		
 		
 		
 		try {
