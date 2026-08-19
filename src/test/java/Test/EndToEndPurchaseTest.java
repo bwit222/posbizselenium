73adc705c41f2bf57ba1856bcd46010ca61c5298
Shubham Singh <shubhamtest222@gmail.com>
2026-08-19 16:25:17 +0530

    Code update

--- src/test/java/Test/EndToEndPurchaseTest.java
+++ src/test/java/Test/EndToEndPurchaseTest.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
